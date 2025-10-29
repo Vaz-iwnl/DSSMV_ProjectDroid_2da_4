52a3d715e709cda94621e212e6f9846ac517e980
AfonsoSobreiroISEP <1241013@isep.ipp.pt>
2025-10-29 13:04:15 +0000

    Fragmentos

--- MainActivity/app/src/main/java/com/example/mainactivity/MainActivity.java
+++ MainActivity/app/src/main/java/com/example/mainactivity/MainActivity.java
@@ -1,17 +1,25 @@
 package com.example.mainactivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ImageButton navReviewsButton, navTrackingButton, navSettingsButton, navHomeButton, navWatchlistButton;
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
@@ -20,5 +28,61 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        navHomeButton = findViewById(R.id.home_button);
+        navReviewsButton = findViewById(R.id.reviews_button);
+        navTrackingButton = findViewById(R.id.tracking_button);
+        navSettingsButton = findViewById(R.id.settings_button);
+        navWatchlistButton = findViewById(R.id.watchlist_button);
+
+        if (savedInstanceState == null) {
+            loadFragment(new HomeFragment());
+        }
+
+        navHomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new HomeFragment());
+            }
+        });
+
+        navSettingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new SettingsFragment());
+            }
+        });
+
+        navReviewsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new ReviewsFragment());
+            }
+        });
+
+        navWatchlistButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new WatchlistFragment());
+            }
+        });
+
+        navTrackingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new TrackingFragment());
+            }
+        });
     }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+
+        fragmentTransaction.commit();
+    }
+
+
 }
