9ff61f25b628f9f9fbd34d26c4cea230e94691bc
marti <1240690@gmail.com>
2025-11-04 15:12:06 +0000

    apis build gradle app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,10 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        buildConfigField("String", "TMDB_API_KEY", "\"${keystoreProperties["TMDB_API_KEY"]}\"")
+        buildConfigField("String", "TMDB_API_KEY", "\"${keystoreProperties["4aaf4923ddf3cfde345af34bb13e4590"]}\"")
+        buildConfigField("String", "RESTDB_API_KEY", "\"${keystoreProperties["c2e3832fda787846d7fb855a3123b1962dce3"]}\"")
+        buildConfigField("String", "TRAKT_CLIENT_ID", "\"${keystoreProperties["e4c406f413fe260bd73e4ca201270f17f45825bf2a1f8094f98220025bb45c79"]}\"")
+        buildConfigField("String", "TRAKT_CLIENT_SECRET", "\"${keystoreProperties["ab7b05b76b96a231f068f6bcb145958a3181f70ef09aa176fcc847e784d3f04d"]}\"")
     }
 
     buildFeatures {
@@ -48,7 +51,6 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.github.bumptech.glide:glide:4.16.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
